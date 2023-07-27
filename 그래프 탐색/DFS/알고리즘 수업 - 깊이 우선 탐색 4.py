291d9dac999365d2e4646e3b57dda6263691a332
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-07-27 17:56:05 +0900

    24482

--- 그래프 탐색/DFS/알고리즘 수업 - 깊이 우선 탐색 4.py
+++ 그래프 탐색/DFS/알고리즘 수업 - 깊이 우선 탐색 4.py
@@ -1,1 +1,30 @@
+import sys
 
+sys.setrecursionlimit(10**6)
+
+n, m, r = map(int, sys.stdin.readline().split())
+
+def dfs(graph, visited, r, cnt):
+    visited[r] = cnt # 시작 정점을 방문 처리
+    # 인접 정점 탐색
+    for i in graph[r]:
+        if visited[i] == -1: # 아직 방문되지 않은 정점이라면
+            dfs(graph, visited, i, cnt+1)
+
+graph = [[] for _ in range(n+1)]
+visited = [-1 for _ in range(n+1)]  # 방문되지 않는 노드의 깊이는 -1
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 내림차순으로 graph 정렬
+for i in range(n+1):
+    graph[i].sort(reverse=True)
+
+cnt = 0
+dfs(graph, visited, r, cnt)
+
+for i in visited[1::]:
+    print(i)
