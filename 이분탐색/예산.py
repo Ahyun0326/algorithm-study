d339032d3a154de9836d16722a4e37ec62305088
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-09-27 17:27:26 +0900

    2512

--- 이분탐색/예산.py
+++ 이분탐색/예산.py
@@ -1,1 +1,27 @@
+import sys
 
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+
+left = 0
+right = max(arr)
+result = 0
+temp = 0
+
+while left <= right:
+    mid = (left+right)//2
+    
+    for i in arr: 
+        temp += min(i, mid)
+    
+    if temp > m:
+        right = mid-1
+        temp = 0
+
+    elif result < m:
+        result = mid
+        left = mid+1
+        temp = 0
+
+print(result)
