3476ffd6aef2d37cda6e38eb0b48ed3c902c5c47
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-10-09 23:52:58 +0900

    9466

--- 그래프 탐색/DFS/텀프로젝트.py
+++ 그래프 탐색/DFS/텀프로젝트.py
@@ -1,1 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
 
+def dfs(curr):
+    global cnt
+    visited[curr] = True
+    next = student[curr]
+
+    if visited[next]:
+        if not finished[next]:
+            for i in range(next, next!=curr):
+                next = student[i]
+                cnt += 1
+            cnt += 1                    
+    else:
+        dfs(next)
+    finished[curr] = True
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    student = [0]
+    student += list(map(int, sys.stdin.readline().split()))
+    visited = [False for _ in range(n+1)]
+    finished = [False for _ in range(n+1)] 
+
+    cnt = 0
+    for i in range(1, n+1):
+        if not visited[i]:
+            dfs(i)
+    print(n-cnt)
