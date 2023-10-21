c6534aaae52e6fdd396ee7c804299fcfd4bb0f94
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-10-22 01:02:35 +0900

    2644

--- 그래프 탐색/BFS/촌수계산.py
+++ 그래프 탐색/BFS/촌수계산.py
@@ -6,29 +6,31 @@ a, b = map(int, sys.stdin.readline().split())
 m = int(sys.stdin.readline())
 
 graph = [[] for i in range(n+1)]
-visited = [0 for _ in range(n+1)]
+visited = [-1 for _ in range(n+1)]
 
 for _ in range(m):
     x, y = map(int, sys.stdin.readline().split())    
     graph[x].append(y)
     graph[y].append(x)
 
-queue = deque([b])
-visited[b] = 0
+queue = deque([a])
+visited[a] = 1
 cnt = 1
-
 while queue:
-    
     v = queue.popleft()
     
+    if v == b:
+        break
+
     for i in graph[v]:
-        if not visited[i]:
-            visited[i] = cnt
+        if visited[i] == -1:
+            visited[i] = visited[v] + cnt
             queue.append(i)
 
-    cnt += 1
-
-if visited[a] == 0:
-    print(-1)
-else:    
-    print(visited[a])
+if visited[b] != -1:
+    if visited[a] > visited[b]:
+        print(visited[a] - visited[b])
+    elif visited[a] < visited[b]:
+        print(visited[b] - visited[a])
+elif visited[b] == -1:
+    print(visited[b])
