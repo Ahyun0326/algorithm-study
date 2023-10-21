9f722622b45e41d5e82c63394262d82c35874a29
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-10-21 23:55:11 +0900

    2644

--- 그래프 탐색/BFS/촌수계산.py
+++ 그래프 탐색/BFS/촌수계산.py
@@ -1,1 +1,34 @@
+import sys
+from collections import deque
 
+n = int(sys.stdin.readline())
+a, b = map(int, sys.stdin.readline().split())
+m = int(sys.stdin.readline())
+
+graph = [[] for i in range(n+1)]
+visited = [0 for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, sys.stdin.readline().split())    
+    graph[x].append(y)
+    graph[y].append(x)
+
+queue = deque([b])
+visited[b] = 0
+cnt = 1
+
+while queue:
+    
+    v = queue.popleft()
+    
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = cnt
+            queue.append(i)
+
+    cnt += 1
+
+if visited[a] == 0:
+    print(-1)
+else:    
+    print(visited[a])
