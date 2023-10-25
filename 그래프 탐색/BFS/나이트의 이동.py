26a660ecc35864d48e7dd747dc88b308ce0a4f80
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-10-25 14:27:10 +0900

    7562

--- 그래프 탐색/BFS/나이트의 이동.py
+++ 그래프 탐색/BFS/나이트의 이동.py
@@ -1,1 +1,32 @@
+import sys
+from collections import deque
 
+t = int(sys.stdin.readline())
+move = [[-2,-1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
+
+for i in range(t):
+    l = int(sys.stdin.readline())
+    start_a, start_b = map(int, sys.stdin.readline().split())
+    end_a, end_b = map(int, sys.stdin.readline().split())
+
+    visited = [[-1 for _ in range(l)] for _ in range(l)]
+    queue = deque([[start_a, start_b]])    
+    visited[start_a][start_b] = 0 # 방문 처리
+
+    result = 0
+    while queue:
+        
+        y, x = queue.popleft()
+        
+        for i in range(8):
+            ny = y + move[i][0] 
+            nx = x + move[i][1]
+        
+            if (0<=ny<l and 0<=nx<l) and visited[ny][nx] == -1:
+                    queue.append([ny, nx])
+                    visited[ny][nx] = visited[y][x] + 1
+
+        if y == end_a and x == end_b:
+            break
+        
+    print(visited[end_a][end_b])       
