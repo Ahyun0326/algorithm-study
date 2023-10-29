3e12f0828df59e793727a1c8c687fb6e3e5c5ac6
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-10-29 12:12:23 +0900

    1449

--- 그리디/수리공항승.py
+++ 그리디/수리공항승.py
@@ -1,1 +1,19 @@
+import sys
 
+# 물이 새는 곳의 개수, 테이프의 길이
+n, l = map(int, sys.stdin.readline().split())
+
+# 물이 새는 위치
+water = list(map(int, sys.stdin.readline().split()))
+
+water.sort()
+start = water[0]
+cnt = 1
+for location in water[1:]:
+    if location in range(start, start+l):
+        continue
+    else:
+        start = location
+        cnt += 1
+
+print(cnt)
