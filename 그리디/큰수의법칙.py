57680d4e96da38718a50a1a76b7b499c7f0d67ef
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-11-08 22:46:18 +0900

    이것이 취업을 위한 코딩 테스트다

--- 그리디/큰수의법칙.py
+++ 그리디/큰수의법칙.py
@@ -1,1 +1,29 @@
+import sys
 
+n, m, k = map(int, sys.stdin.readline().split())
+
+number = list(map(int,sys.stdin.readline().split()))
+
+# 내림차순 정렬
+number.sort(reverse=True)
+
+result = 0
+for i in range(m):
+
+    # 가장 큰 수가 중복되지 않는 경우
+    if number.count(number[0]) == 1:
+        
+        # k번 더했을 경우 그 두 번째로 큰 수를 더하기
+        if (i+1)%k == 0:
+            result += number[1]
+            continue
+
+        # 제일 큰 수 더하기
+        result += number[0]
+    
+    # 가장 큰 수가 중복되는 경우
+    elif number.count(number[0]) >= 2:
+        # k값에 상관없이 가장 큰 수 계속 더하기
+        result += number[0] 
+
+print(result)
