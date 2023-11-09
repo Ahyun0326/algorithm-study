bf081707e173d963888c0fb6a093b26423fad428
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-11-10 00:06:28 +0900

    2212

--- 그리디/센서.py
+++ 그리디/센서.py
@@ -1,1 +1,23 @@
+import sys
 
+n = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
+
+sensor = list(map(int, sys.stdin.readline().split()))
+sensor.sort()
+
+result = []
+for i in range(n-1):
+    if n == 1:
+        result.append(0)
+        break
+    result.append(sensor[i+1] - sensor[i])
+
+result.sort(reverse=True)
+for i in range(k-1):
+    if n == 1:
+        break
+    
+    result.pop(0)    
+
+print(sum(result))
