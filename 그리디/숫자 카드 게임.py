34d1113b796ffdfe4816ef9cfda4fd1a024933b0
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-11-11 01:18:00 +0900

    이것이 취업을 위한 코딩 테스트다

--- 그리디/숫자 카드 게임.py
+++ 그리디/숫자 카드 게임.py
@@ -1,1 +1,13 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+card = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+result = []
+for i in range(n):
+
+    result.append(min(card[i]))
+
+print(max(result))
 
