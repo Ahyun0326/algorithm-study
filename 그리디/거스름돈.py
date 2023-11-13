69a030736e31d3eb4c7aecce6cd510e16a18110f
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-11-13 23:14:10 +0900

    이것이 취업을 위한 코딩 테스트다 3-1

--- 그리디/거스름돈.py
+++ 그리디/거스름돈.py
@@ -1,1 +1,25 @@
+import sys
 
+n = int(sys.stdin.readline())
+
+cnt = 0
+
+while n > 0:
+
+    if n//500 > 0:
+        cnt += n//500
+        n %= 500
+
+    elif n//100 > 0:
+        cnt += n//100
+        n %= 100
+
+    elif n//50 > 0:
+        cnt += n//50
+        n %= 50
+    
+    elif n//10 > 0:
+        cnt += n//10
+        n %= 10
+
+print(cnt)
