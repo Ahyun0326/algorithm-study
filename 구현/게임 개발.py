7f9bbacf4e9252b2788f1e970bc3d4cb6a0fa109
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-11-18 00:40:40 +0900

    이것이 취업을 위한 코딩테스트다

--- 구현/게임 개발.py
+++ 구현/게임 개발.py
@@ -1,1 +1,54 @@
+import sys
 
+n, m = map(int, sys.stdin.readline().split())
+a, b, d = map(int, sys.stdin.readline().split())
+
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+# 북, 동, 남, 서
+dx = [-1, 0, 1, 0] 
+dy = [0, 1, 0 ,-1]
+
+visited[a][b] = True
+cnt = 0
+turn_time = 0
+while True:
+
+    d -= 1 # 반시계 방향으로 회전
+    if d == -1:
+        d = 3
+
+    nx = a + dx[d]
+    ny = b + dy[d]
+
+    # 가보지 않은 칸이면 회전한 방향으로 전진
+    if graph[nx][ny] == 0 and visited[nx][ny] == False:
+        visited[nx][ny] = True # 방문 처리
+        a = nx
+        b = ny
+        cnt += 1
+        turn_time = 0
+        continue 
+
+    # 가보지 않은 칸이 없다면 왼쪽으로 회전만
+    else:
+        turn_time += 1
+
+    # 네 방향 모두 이미 가본 칸이거나 바다라면
+    if turn_time == 4:
+        nx = a - dx[d]
+        ny = b - dy[d]
+
+        # 바라보는 방향 유지한 채로 뒤로 가기
+        if graph[nx][ny] == 0:
+            a = nx
+            b = ny
+
+        # 뒤로 갈 수 없는 경우 움직임 중단
+        else:
+            break
+
+        turn_time = 0
+
+print(cnt)
