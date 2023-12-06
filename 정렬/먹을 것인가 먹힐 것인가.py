6f9680945ca60829480a813a449771ed4609fc2e
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-12-06 21:14:46 +0900

    [백준 7795]

--- 정렬/먹을 것인가 먹힐 것인가.py
+++ 정렬/먹을 것인가 먹힐 것인가.py
@@ -1,1 +1,26 @@
+import sys
 
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    n, m = map(int,sys.stdin.readline().split())
+    # a, b 입력 받기
+    A = list(map(int,sys.stdin.readline().split()))
+    B = list(map(int,sys.stdin.readline().split()))
+
+    # a, b 리스트 내림차순 정렬
+    A.sort()    
+    B.sort()
+
+    start = 0
+    cnt = 0
+
+    for j in range(n):
+        while True:
+            if start == m or A[j]<=B[start]:
+                cnt += start
+                break
+            else:
+                start += 1
+
+    print(cnt)
