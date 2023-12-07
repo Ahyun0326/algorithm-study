be6cad00547aff4d1e62bf2ddfa575bef3016733
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-12-07 18:29:42 +0900

    이코테

--- 정렬/위에서 아래로.py
+++ 정렬/위에서 아래로.py
@@ -1,1 +1,13 @@
+n = int(input())
 
+nums = []
+for i in range(n):
+    num = int(input())
+    nums.append(num)
+
+# 내림차순 정렬
+nums.sort(reverse=True)
+
+# 한 줄에 공백으로 구분하여 출력
+for i in nums:
+    print(i, end=' ')
