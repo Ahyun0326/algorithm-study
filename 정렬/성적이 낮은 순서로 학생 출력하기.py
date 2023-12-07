0d17758d68b379619e3a2dc6cb0062132bc70407
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-12-07 18:30:08 +0900

    이코테

--- 정렬/성적이 낮은 순서로 학생 출력하기.py
+++ 정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -1,1 +1,16 @@
+n = int(input())
+
+students = []
+# 학생의 이름, 점수를 입력 받아 students 리스트에 저장
+for i in range(n):
+    student = input().split()
+    # 점수는 정수형으로 변환해 추가하기
+    students.append([student[0], int(student[1])])
+
+# sorted와 lambda를 이용해 점수를 기준으로 students 리스트 정렬
+answer = sorted(students, key=lambda x:x[1])
+print(answer)
+
+
+
 
