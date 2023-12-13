b9c45ff03760dca8c06d266e7521b617b9368f57
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-12-13 16:10:56 +0900

    이코테

--- 이분탐색/부품찾기.py
+++ 이분탐색/부품찾기.py
@@ -1,1 +1,34 @@
+import sys
+
+def binary_search(target, store, start, end):
+    start = 0
+    end = len(store)
+    while start <= end:
+        
+        mid = (start+end) // 2
+        # 고객이 찾는 부품이 있다면 yes 출력
+        if store[mid] == i:
+            print("yes", end=' ')
+            return
+        # 고객이 찾는 부품이 크면 오른쪽 탐색 
+        elif store[mid] < target:
+            start = mid+1
+        # 고객이 찾는 부품이 작으면 왼쪽 탐색
+        else:
+            end = mid-1
+    # 고객이 찾는 부품이 없으면 no 출력
+    print("no", end= ' ')
+    return
+
+
+n = int(sys.stdin.readline())
+store = list(map(int, sys.stdin.readline().split()))
+
+m = int(sys.stdin.readline())
+customer = list(map(int, sys.stdin.readline().split()))
+
+# 가게에 있는 부품의 번호 오름차순 정렬
+store = sorted(store)
+for i in customer:
+    binary_search(i, store, 0, n-1)
 
