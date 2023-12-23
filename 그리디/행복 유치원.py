b78721aa1b6a9ed95364a97010d8b846830d8023
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2023-12-24 00:01:33 +0900

    [백준 13164]

--- 그리디/행복 유치원.py
+++ 그리디/행복 유치원.py
@@ -1,1 +1,13 @@
+import sys
 
+# 원생들의 수, 조의 개수 입력받기
+n, k = map(int, sys.stdin.readline().split())
+# 원생들의 키를 원생들의 수만큼 입력받기
+kids = list(map(int, sys.stdin.readline().split()))
+
+sub = []
+for i in range(n-1):
+    sub.append(kids[i+1] - kids[i])
+
+sub.sort()
+print(sum(sub[0:n-k]))
