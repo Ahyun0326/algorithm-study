e70bc5546de6b0b387237072154f66c1ec91daa1
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2024-01-01 22:50:41 +0900

    [이코테]

--- dp/개미 전사.py
+++ dp/개미 전사.py
@@ -1,1 +1,16 @@
+n = int(input())
+store = list(map(int, input().split()))
 
+dp = [0] * 100
+dp[0] = store[0]
+dp[1] = max(store[0], store[1])
+
+for i in range(2, n):
+
+    dp[i] = store[i] + dp[i-2]
+
+    # 현재 선택된 창고와 인접한 곳이 이미 약탈당했다면
+    if dp[i-1] != 0:
+        dp[i] = max(dp[i], dp[i-1]) # 둘 중에서 큰 값을 저장
+
+print(dp[n-1])
