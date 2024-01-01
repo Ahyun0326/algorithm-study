bdd975f2e6378aa6e854c0cf459070941c2640cc
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2024-01-02 00:33:19 +0900

    [백준 2785]

--- 그리디/체인.py
+++ 그리디/체인.py
@@ -1,1 +1,20 @@
+import sys
 
+n = int(sys.stdin.readline())
+chain = list(map(int, sys.stdin.readline().split()))
+
+# 체인 오름차순 정렬
+chain.sort()
+tiredchains = 1
+chaincount = n
+
+# n만큼 반복
+for i in range(n):
+
+    if tiredchains + chain[i] >= chaincount:
+        break
+    else:
+        tiredchains += chain[i]
+        chaincount -= 1
+
+print(chaincount-1)
