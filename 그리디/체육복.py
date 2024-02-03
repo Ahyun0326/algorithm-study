b280d479718b0ba8cb324c6da812cea4379a4c92
yeseo <103279191+Ahyun0326@users.noreply.github.com>
2024-02-03 16:59:37 +0900

    [프로그래머스 level1]

--- 그리디/체육복.py
+++ 그리디/체육복.py
@@ -1,1 +1,18 @@
+def solution(n, lost, reserve):
+    
+    # n: 총 학생 수
+    # lost: 도난당한 학생 번호
+    # reserve: 여분이 있는 학생 번호
+    
+    # 여벌 체육복을 가지고 있는 학생이 도난당한 경우는 제외
+    _lost = set(lost) - set(reserve)
+    _reserve = set(reserve) - set(lost)
 
+    for i in _reserve:
+
+        if i-1 in _lost:
+            _lost.remove(i-1)
+        elif i+1 in _lost:
+            _lost.remove(i+1)
+
+    return n-len(_lost)
