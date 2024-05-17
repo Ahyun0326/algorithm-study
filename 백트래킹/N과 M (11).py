37550a5073bea32b70f1e30cdffaad0f3e6a5489
Yeseo <chinkl0110@naver.com>
2024-05-17 16:26:38 +0900

    solve : 16555 N과M(11)

---
+++ 백트래킹/N과 M (11).py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(data):
+    if len(data) == m:
+        print(*data)
+        return
+
+    for i in range(len(array)):
+        data.append(array[i])
+        dfs(data)
+        data.pop()
+
+n, m = map(int, input().split())
+array = sorted(set(list(map(int, input().split()))))
+data = []
+
+dfs(data)
